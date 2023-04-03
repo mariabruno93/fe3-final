27a9e56491c8b3e85ef2ca752ca9b19ae72a5fc5
Maria Bruno <brunomariaok@gmail.com>
2023-04-03 16:03:40 -0300

    firsh commit

--- src/Components/utils/global.context.jsx
+++ src/Components/utils/global.context.jsx
@@ -7,6 +7,8 @@ export const ContextGlobal = createContext(undefined);
 export const ContextProvider = ({ children }) => {
   //Aqui deberan implementar la logica propia del Context, utilizando el hook useMemo
 
+  const themeLocalValue= JSON.parse(localStorage.getItem("theme"))
+
   return (
     <ContextGlobal.Provider value={{}}>
       {children}
