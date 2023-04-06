6a22a9bb4b488a20cf0ff8fedbe823d70c4d81ec
Maria Bruno <brunomariaok@gmail.com>
2023-04-05 22:22:11 -0300

    favoritos

--- src/Routes/Favs.jsx
+++ src/Routes/Favs.jsx
@@ -1,22 +1,29 @@
 import React, { useContext } from "react";
 import Card from "../Components/Card";
 import { ContextGlobal } from "../Components/utils/global.context"
+import { Link } from "react-router-dom";
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Favs = () => {
   const {state, dispatchFavs,data} = useContext(ContextGlobal)
-  console.log(state)
-  console.log(data)
-  
+  const stateArray= state.map((estado)=>estado.id)
+  const favsFiltrados=data.filter(dentista=>stateArray.includes(dentista.id))
   return (
     <>
       <h1>Dentists Favs</h1>
       <div className="card-grid">
         {/* este componente debe consumir los destacados del localStorage */}
-        {state?.map((fav)=>(
-                <div key={fav.id}>
-                  <Card {...data} data={data}/>
-                </div>
+        {favsFiltrados.map((fav)=>(
+          <div key={fav.id}>
+            <div className="card" id={fav.id}>
+              <Link to={`/dentist/${fav.id}`}>
+                <h2>{fav.name}</h2>
+                <img className="card-img-top"
+                src="/images/doctor.jpg"
+                alt="doctor placeholder" />
+              </Link>
+            </div>
+          </div>
         ))}
         {/* Deberan renderizar una Card por cada uno de ellos */}
       </div>
